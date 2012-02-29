6d523d408bcd1fb635543972dd5331df6fe87eb7
Nicolas Echavarria <nicolasechavarria@youremail.com>
2012-02-29 17:25:20 -0600

    This is the first commit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,7 +12,7 @@ class BootStrap {
                         dateOfBirth: (new Date() - 365 * 30),
                         gender: "F",
                         address: "123 Main St",
-                        city: "Goose",
+                        city: "Gooses",
                         state: "NC",
                         zipcode: "12345",
                         email: "jane@whereever.com"
